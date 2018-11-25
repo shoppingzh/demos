82142e740a26577264ad4dd040a8c37d2501222b
shoppingzh <1049262057@qq.com>
2018-11-25 16:51:16 +0800

    整合thymeleaf

--- springboot-ssm/src/main/java/com/littlezheng/springbootssm/controller/UserController.java
+++ springboot-ssm/src/main/java/com/littlezheng/springbootssm/controller/UserController.java
@@ -1,17 +1,32 @@
 package com.littlezheng.springbootssm.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.littlezheng.springbootssm.dao.UserMapper;
+import com.littlezheng.springbootssm.entity.User;
 
 @Controller
 @RequestMapping(path = "user")
 public class UserController {
+	
+	@Resource
+	private UserMapper userMapper;
+	
+	@GetMapping(path = "index")
+	public String index(){
+		return "user/index";
+	}
 
-	@GetMapping(path = "/")
-	public ModelAndView index(){
-		return new ModelAndView("user/index");
+	@GetMapping(path = "")
+	public @ResponseBody List<User> listAll(){
+		return userMapper.selectAll();
 	}
 	
 }
